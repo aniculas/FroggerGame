727bf251d79510c39e6c1c473947ee5f64d0d07c
JMixon7 <JMixon7@gatech.edu>
2023-03-07 13:19:32 -0500

    Player class implemented, broken car movement removed

--- app/src/main/java/com/example/myapplication/Gameplay.java
+++ app/src/main/java/com/example/myapplication/Gameplay.java
@@ -10,19 +10,15 @@ import static com.example.myapplication.Config.getName;
 import static com.example.myapplication.Config.getRed;
 import static com.example.myapplication.Config.getSprite;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.concurrent.ExecutorService;
 
 
 public class Gameplay extends AppCompatActivity {
@@ -31,10 +27,7 @@ public class Gameplay extends AppCompatActivity {
     float downx;
     float upy;
     float downy;
-    float outPX = 970;
-    float outNX = -50;
-    float outPY = 1900;
-    float outNY = 800;
+    Player player;
     int maxHeight = 0;
     int height = 0;
     int score = 0;
@@ -71,23 +64,17 @@ public class Gameplay extends AppCompatActivity {
             diff.setText("Hard");
         }
         if (spritepicker.getCheckedRadioButtonId() == green.getId()) {
-            playerspriteg.setVisibility(View.VISIBLE);
+            player = new Player(playerspriteg);
         } else if (spritepicker.getCheckedRadioButtonId() == red.getId()) {
-            playerspriter.setVisibility(View.VISIBLE);
+            player = new Player(playerspriter);
         } else if (spritepicker.getCheckedRadioButtonId() == blue.getId()) {
-            playerspriteb.setVisibility(View.VISIBLE);
+            player = new Player(playerspriteb);
         }
         TextView playerName = (TextView) findViewById(R.id.playername);
         playerName.setText(getName().getText());
         // Background
         int numRoads = (int) (Math.random() * 2);
         backgroundCreate(numRoads);
-        carAnimation(slowcar1, 3000);
-        slowcar2.setX(slowcar2.getX() + 400);
-        carAnimation(slowcar2, 3000);
-        slowcar3.setX(slowcar3.getX() + 800);
-        carAnimation(slowcar3, 3000);
-        //ExecutorService executor
     }
 //    protected void difficultySelect() {
 //
@@ -130,19 +117,6 @@ public class Gameplay extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
-        int yMovement = 120;
-        int xMovement = 109;
-        ImageView playersprite;
-        ImageView playerspriteg = (ImageView) findViewById(R.id.playerspriteg);
-        ImageView playerspriter = (ImageView) findViewById(R.id.playerspriter);
-        ImageView playerspriteb = (ImageView) findViewById(R.id.playerspriteb);
-        if (playerspriteg.getVisibility() == View.VISIBLE) { //checks which sprite was selected
-            playersprite = playerspriteg;
-        } else if (playerspriter.getVisibility() == View.VISIBLE) {
-            playersprite = playerspriter;
-        } else {
-            playersprite = playerspriteb;
-        }
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: //when swipe started
                 downx = event.getX(); //gets x of swipe start
@@ -151,29 +125,7 @@ public class Gameplay extends AppCompatActivity {
             case MotionEvent.ACTION_UP: //when swipe ended
                 upx = event.getX(); //gets x of swipe end
                 upy = event.getY(); //gets y of swipe end
-                float countPX = playersprite.getX() + xMovement;
-                float countNX = playersprite.getX() - xMovement;
-                float countPY = playersprite.getY() + yMovement;
-                float countNY = playersprite.getY() - yMovement;
-                float deltax = downx - upx;
-                float deltay = downy - upy;
-                if (Math.abs(deltax) > Math.abs(deltay)) { //checks if swipe was vertical or horizontal
-                    if (downx > upx && countNX > outNX) { // checks if swipe was left to right
-                        playersprite.setX(playersprite.getX() - xMovement);
-                    }
-                    if (downx < upx && countPX < outPX){
-                        playersprite.setX(playersprite.getX() + xMovement);
-                    }
-                } else {
-                    if (downy > upy && countNY > outNY) { //checks if swipe was top to bottom
-                        playersprite.setY(playersprite.getY() - yMovement);
-                        height++;
-                        setScore();
-                    } if (downy < upy && (countPY < outPY)) {
-                        playersprite.setY(playersprite.getY() + yMovement);
-                        height--;
-                    }
-                }
+                height = player.move(upx,upy,downx, downy);
                 break;
         }
 
@@ -186,12 +138,5 @@ public class Gameplay extends AppCompatActivity {
             score = score + 10;
             scoreText.setText("Score: " + score);
         }
-    }
-    public void carAnimation(ImageView car, long carSpeed) {
-        TranslateAnimation moveCar = new TranslateAnimation(100, -1200, 0, 0);
-        moveCar.setDuration(carSpeed);
-        moveCar.setFillAfter(true);
-        moveCar.setRepeatCount(-1);
-        car.startAnimation(moveCar);
     }
 }
