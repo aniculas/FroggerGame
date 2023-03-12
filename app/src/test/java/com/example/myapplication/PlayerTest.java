8ab0a547c81ab98c647899f8f11659d743b8c2d3
anicuas3 <aniculas3@gatech.edu>
2023-03-12 01:56:07 -0500

    "5 tests complete for scores"

--- app/src/test/java/com/example/myapplication/PlayerTest.java
+++ app/src/test/java/com/example/myapplication/PlayerTest.java
@@ -13,41 +13,48 @@ public class PlayerTest {
     public void SetUp() {
         player = new Player(478f,1870f);
     }
-//    @Test
-//    public void downScore() {
-//        int iScore = player.score;
-//        player.move(0,1f,0,0,478f, 1870f);
-//        int fScore = player.score;
-//        Assert.assertTrue(iScore == fScore);
-//    }
+    @Test
+    public void downScore() {
+        int iScore = player.score;
+        player.move(0,100f,0,0,478f, 1870f);
+        player.scoreReturn(false);
+        int fScore = player.score;
+        Assert.assertTrue(iScore == fScore);
+    }
     @Test
     public void upScore() {
         int iScore = player.score;
-        player.move(0,2000f,0,1700f,478f,1870f);
+        player.move(0f,0f,0f,100f,478f,1870f);
+        player.scoreReturn(false);
         int fScore = player.score;
-        Assert.assertTrue(5 < 6);
+        Assert.assertTrue( iScore < fScore);
     }
-//    @Test
-//    public void leftScore() {
-//        int iScore = player.score;
-//        player.move(0,0,0,0);
-//        int fScore = player.score;
-//        Assert.assertTrue(iScore == fScore);
-//    }
-//    @Test
-//    public void rightScore() {
-//        int iScore = player.score;
-//        player.move(0,0,0,0);
-//        int fScore = player.score;
-//        Assert.assertTrue(iScore == fScore);
-//    }
-//    @Test
-//    public void safeSpace(){
-//        int iScore = player.score;
-//        player.move(0,0,0,0);
-//        player.move(0,0,0,0);
-//        player.move(0,0,0,0);
-//        int fScore = player.score;
-//        Assert.assertTrue(iScore == fScore);
-//    }
+    @Test
+    public void leftScore() {
+        int iScore = player.score;
+        player.move(100,0,0,0,478f, 1870f);
+        player.scoreReturn(false);
+        int fScore = player.score;
+        Assert.assertTrue(iScore == fScore);
+    }
+    @Test
+    public void rightScore() {
+        int iScore = player.score;
+        player.move(0,0,0,100f,478f, 1870f);
+        player.scoreReturn(false);
+        int fScore = player.score;
+        Assert.assertTrue(iScore == fScore);
+    }
+    @Test
+    public void fastPoints(){
+        int eScore = 35;
+        player.move(0,0,0,100f,478f, 1870f);
+        player.scoreReturn(false);
+        player.move(0,0,0,100f,player.xPos, player.yPos);
+        player.scoreReturn(false);
+        player.move(0,0,0,100f,player.xPos, player.yPos);
+        player.scoreReturn(false);
+        int fScore = player.score;
+        Assert.assertTrue(eScore == fScore);
+    }
 }
